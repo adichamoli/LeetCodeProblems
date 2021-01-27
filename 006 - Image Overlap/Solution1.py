25f588f1ee2b17e1ff5697c479080523458cc8af
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-27 12:44:13 +0530

    LeetCodeProblems

---
+++ 006 - Image Overlap/Solution1.py
@@ -0,0 +1,21 @@
+'''59 / 59 test cases passed.                              Status: Accepted
+Runtime: 680 ms                                            Memory Usage: 14.7 MB'''
+
+class Solution:
+    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+        d1 = collections.defaultdict(int)
+        temp1 = []
+        temp2 = []
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if(A[i][j] == 1):
+                    temp1.append((i,j))
+                if(B[i][j] == 1):
+                    temp2.append((i,j))
+        res = 0
+        for a in temp1:
+            for b in temp2:
+                c = (b[0]-a[0],b[1]-a[1])
+                d1[c] += 1
+                res = max(res, d1[c])
+        return res
