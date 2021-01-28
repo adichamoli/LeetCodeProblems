2f8938301482f6d4138d3b093df524334ed52e7f
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-28 12:01:03 +0530

    LeetCodeProblems

---
+++ 007 - Word Pattern/Solution1.py
@@ -0,0 +1,25 @@
+'''36 / 36 test cases passed.                    Status: Accepted
+Runtime: 32 ms                                   Memory Usage: 14.4 MB'''
+
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
+        map_char = {}
+        map_word = {}
+        
+        words = str.split(' ')
+        
+        if len(words)!= len(pattern):
+            return False
+        
+        for c,w in zip(pattern, words):
+            if c not in map_char:
+                if w in map_word:
+                    return False
+                else:
+                    map_char[c] = w
+                    map_word[w] = c
+                    
+            else:
+                if map_char[c] != w:
+                    return False
+        return True
